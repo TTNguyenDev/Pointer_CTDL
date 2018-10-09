920df8a3a744a957008d36af643a313171779bae
TTNguyen <nttriet9a4@gmail.com>
2018-10-09 10:10:53 +0700

    5 Exercises

--- Pointer/main.cpp
+++ Pointer/main.cpp
@@ -1,15 +1,56 @@
-//
-//  main.cpp
-//  Pointer
-//
-//  Created by TT Nguyen on 10/9/18.
-//  Copyright © 2018 TT Nguyen. All rights reserved.
-//
-
 #include <iostream>
+#include <stdio.h>
+#include <math.h>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+using namespace std;
+
+void inputArray(int* &a, int &n) {
+    for (int i = 0; i < n; i++) {
+        cout << "Enter a[" << i << "] = ";
+        cin >> a[i];
+    }
 }
+
+void deallocateArray(int* &a) {
+    delete []a;
+}
+
+void printArray(int* &a, int n) {
+    cout <<"Array a: " << endl;
+    for (int i = 0; i < n; i++) {
+        cout << "a[" << i << "] = " << a[i] << endl;
+    }
+}
+
+int findMin(int* a, int n) {
+    int min = a[0];
+    for (int i = 1; i < n; i++) {
+        if (min > a[i])
+            min = a[i];
+    }
+    return min;
+}
+
+int findMaxAbsElement(int* a, int n) {
+    int maxABS = abs(a[0]);
+    for (int i = 1; i < n; i++) {
+        if (maxABS < abs(a[i]))
+            maxABS = abs(a[i]);
+    }
+    return maxABS;
+}
+
+int main() {
+    int *a;
+    int n;
+   
+    cout << "Enter number of elements: ";
+    cin >> n;
+    
+    a = new int[n];
+    
+    inputArray(a, n);
+    printArray(a, n);
+    deallocateArray(a);
+}
+
